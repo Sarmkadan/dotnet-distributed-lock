def1d7836adb182bec3e14a950eee4ea5dc2a060
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 20:19:25 +0300

    chore: tweak InMemoryLockRepositoryTests.

---
+++ tests/dotnet-distributed-lock.Tests/InMemoryLockRepositoryTests.cs
@@ -0,0 +1,72 @@
+using Xunit;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using System;
+
+namespace dotnet_distributed_lock.Tests
+{
+    public class InMemoryLockRepositoryTests
+    {
+        [Fact]
+        public async Task TestAcquireAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetByKeyAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetByKeyAndOwnerAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestUpdateAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestRenewAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestReleaseAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestExistsAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetAllActiveLockAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetByOwnerAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestDeleteExpiredLockAsync()
+        {
+            // Test implementation
+        }
+    }
+}
