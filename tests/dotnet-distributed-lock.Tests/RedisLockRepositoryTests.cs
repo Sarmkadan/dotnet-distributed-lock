4e9cf64544bdaddcb930f72963fc59ae680632e8
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-29 06:56:40 +0300

    Wip

---
+++ tests/dotnet-distributed-lock.Tests/RedisLockRepositoryTests.cs
@@ -0,0 +1,67 @@
+using Xunit;
+using Moq;
+using System.Threading;
+using System.Collections.Generic;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+using System;
+
+namespace dotnet_distributed_lock.Tests
+{
+    public class RedisLockRepositoryTests
+    {
+        [Fact]
+        public async Task TestAcquireAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetByKeyAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetByKeyAndOwnerAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestUpdateAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestRenewAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestReleaseAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestExistsAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetAllActiveLockAsync()
+        {
+            // Test implementation
+        }
+
+        [Fact]
+        public async Task TestGetByOwnerAsync()
+        {
+            // Test implementation
+        }
+    }
+}
