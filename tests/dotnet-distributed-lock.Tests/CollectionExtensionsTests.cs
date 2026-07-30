606b4f70b71c37fe806995bbf9db586179b250a4
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 12:52:52 +0300

    CollectionExtensionsTests changes

---
+++ tests/dotnet-distributed-lock.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SarmKadan.DistributedLock.Utilities.Extensions;
+
+namespace dotnet_distributed_lock.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        // ... entire file content ...
+    }
+}
