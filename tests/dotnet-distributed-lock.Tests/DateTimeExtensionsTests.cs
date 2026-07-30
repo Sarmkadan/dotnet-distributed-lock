9bf6e5cda939a72279e73675639d1a0a52fd8bbd
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 21:30:00 +0300

    chore: DateTimeExtensionsTests changes.

---
+++ tests/dotnet-distributed-lock.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,50 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace dotnet_distributed_lock.Tests
+{
+    public class DateTimeExtensionsTests
+    {
+        [Fact]
+        public void HappyPath_IsExpired()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_IsExpired_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_IsExpired_InvalidInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_IsValid()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_IsValid_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_IsValid_InvalidInput()
+        {
+            // Test implementation here
+        }
+    }
+}
