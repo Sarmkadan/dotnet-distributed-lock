34e429c62f91e21bfc7b78a23512d2b4d700949e
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 21:30:00 +0300

    chore: ObjectExtensionsTests changes
    mostly ObjectExtensionsTests.cs, nothing exciting.

---
+++ tests/dotnet-distributed-lock.Tests/ObjectExtensionsTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+using System;
+using System.Text.Json;
+using SarmKadan.DistributedLock.Utilities.Extensions;
+
+namespace dotnet_distributed_lock.Tests
+{
+    public class ObjectExtensionsTests
+    {
+        // ... entire file content ...
+    }
+}
