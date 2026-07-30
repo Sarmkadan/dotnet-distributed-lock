fa1f722eea54b97262b901a2072cce25ac5d5839
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 15:58:12 +0300

    .
    should be backwards compatible.

---
+++ tests/dotnet-distributed-lock.Tests.StringExtensionsTests.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xunit;
+using Moq;
+
+namespace dotnet_distributed_lock.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void IsValidLockName_HAPPY_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void SanitizeForLockName_HAPPY_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ToUtf8Bytes_HAPPY_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ToAsciiBytes_HAPPY_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void FromHexString_HAPPY_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValidGuid_HAPPY_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TruncateWithEllipsis_HAPPY_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ToTrimmedList_HAPPY_PATH()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValidLockName_NULL_INPUT()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void SanitizeForLockName_NULL_INPUT()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ToUtf8Bytes_NULL_INPUT()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ToAsciiBytes_NULL_INPUT()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void FromHexString_NULL_INPUT()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsValidGuid_NULL_INPUT()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void TruncateWithEllipsis_NULL_INPUT()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ToTrimmedList_NULL_INPUT()
+        {
+            // Test implementation here
+        }
+    }
+}
