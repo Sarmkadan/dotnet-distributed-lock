363e074108db7a6d42d321a4ea631586354a3cdc
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-31 21:30:00 +0300

    fix: fix the build

---
+++ tests/dotnet-distributed-lock.Tests/ValidationHelperValidationTests.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using System;
+
+namespace dotnet_distributed_lock.Tests
+{
+    public class ValidationHelperValidationTests
+    {
+        [Fact]
+        public async Task Test_Validate_Happy_Path()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task Test_Validate_Null_Input()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task Test_Validate_Empty_Collection()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task Test_Validate_Boundary_Values()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task Test_Validate_Expected_Exceptions()
+        {
+            // Test implementation here
+        }
+    }
+}
