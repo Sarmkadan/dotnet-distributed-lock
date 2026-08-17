b9504eecb296e46ef811c5f046939c91ddeda965
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-17 20:10:22 +0000

    chore: apply workspace updates

---
+++ tests/dotnet-distributed-lock.Tests/LockRenewalWorkerExtensionsTests.cs
@@ -0,0 +1,86 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace dotnet_distributed_lock.Tests
+{
+    public class LockRenewalWorkerExtensionsTests
+    {
+        [Fact]
+        public void HappyPath_TryRegisterForRenewal()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_TryRegisterForRenewal_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_TryRegisterForRenewal_InvalidInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_SafeUnregisterFromRenewal()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_SafeUnregisterFromRenewal_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_SafeUnregisterFromRenewal_InvalidInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_GetTimeUntilNextRenewal()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_GetTimeUntilNextRenewal_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_GetTimeUntilNextRenewal_InvalidInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_TryUpdateRenewalInterval()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_TryUpdateRenewalInterval_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_TryUpdateRenewalInterval_InvalidInput()
+        {
+            // Test implementation here
+        }
+    }
+}
