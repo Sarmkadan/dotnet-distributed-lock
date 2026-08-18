fdbb6fe8da79bd43688127ad9f6665aeaa7a9315
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-18 03:24:18 +0000

    chore: apply workspace updates

---
+++ tests/dotnet-distributed-lock.Tests/CsvLockExporterTests.cs
@@ -0,0 +1,86 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace dotnet_distributed_lock.Tests
+{
+    public class CsvLockExporterTests
+    {
+        [Fact]
+        public void HappyPath_ExportLock()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_ExportLock_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_ExportLock_InvalidInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_ExportLocks()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_ExportLocks_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_ExportLocks_InvalidInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_ExportLocksToStreamAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_ExportLocksToStreamAsync_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_ExportLocksToStreamAsync_InvalidInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_ExportMetrics()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_ExportMetrics_NullInput()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_ExportMetrics_InvalidInput()
+        {
+            // Test implementation here
+        }
+    }
+}
